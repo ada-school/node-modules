368c7d9e69f371471a11da25992e3131df26731b
Sergio Marentes <sergiomarentes705@gmail.com>
2022-11-22 12:02:06 -0500

    Add chalk to colorize the console logs messages

--- calculadoraModulo.mjs
+++ calculadoraModulo.mjs
@@ -1,5 +1,21 @@
 // importación en modulos de JS
 
 import { resta } from "./restaModulo.mjs";
+import chalk from "chalk";
 
 console.log(resta(5, 3));
+
+const primerResultado = resta(5, 3);
+
+const segundoResultado = resta(10, 4);
+
+const mostrarResultado = (resultado) => {
+  if (resultado < 5) {
+    console.log(chalk.red.bgGray(resultado));
+  } else {
+    console.log(chalk.green.bgGray(resultado));
+  }
+};
+
+mostrarResultado(primerResultado);
+mostrarResultado(segundoResultado);
